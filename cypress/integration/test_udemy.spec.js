585e1fa33fbb80def19d8a96976e02d9c1891eb3
Alla Sapuga <as@logiceditor.com>
2021-08-24 12:46:34 +0300

    contains checking

--- cypress/integration/test_udemy.spec.js
+++ cypress/integration/test_udemy.spec.js
@@ -1,1 +1,21 @@
 /// <reference types="Cypress" />
+
+it('General search', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Sign in')
+})
+
+it('Using tag', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div','Sign in')
+})
+
+it('Ignoring case', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Sign In', {matchCase: false})
+})
+
+it.only('Ignoring case', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('footer').contains('Regulations and tariffs', {matchCase: false})
+})
