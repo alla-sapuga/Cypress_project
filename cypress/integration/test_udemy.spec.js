4bfc33823207a4696fc6fc46249cced790e15b4d
Alla Sapuga <as@logiceditor.com>
2021-08-27 12:42:18 +0300

    add optimized transfer test

--- cypress/integration/test_udemy.spec.js
+++ cypress/integration/test_udemy.spec.js
@@ -1,10 +1,12 @@
 /// <reference types="Cypress" />
 import { mobileReplenishment } from "../support/pages/mobileReplenishment"
+import { transfers } from "../support/pages/transfers"
+
 // import * as mobileReplenishment from "../support/pages/mobileReplenishment"
 
 
 
-it.only ('Replenishment of Ukraine phone number', () => {
+it ('Replenishment of Ukraine phone number', () => {
     cy.visit('https://next.privat24.ua/mobile?lang=en')
 
     mobileReplenishment.typePhoneNumber('686979712')
@@ -23,57 +25,24 @@ it.only ('Replenishment of Ukraine phone number', () => {
 })
 
 
-it ('Money transfer between foreign cards', () => {
+it.only ('Money transfer between foreign cards', () => {
     cy.visit('https://next.privat24.ua/money-transfer/card?lang=en')
-        .wait(3000)
-        .get('[data-qa-node="numberdebitSource"]')
-        .type('4552331448138217')
+    cy.wait(3000)
 
-        .get('[data-qa-node="expiredebitSource"]')
-        .type('0524')
+    transfers.typeDebitCardData('4552331448138217','0524','111')
+    transfers.typeName('taras','schevchenko')
+    transfers.typeReceiverCard('5309233034765085')
+    transfers.typeAmount('300')
+    transfers.typeComment('Cypress test')
+    cy.wait(3000)
+    transfers.submitPayment()
 
-        .get('[data-qa-node="cvvdebitSource"]')
-        .type('111')
-
-        .get('[data-qa-node="firstNamedebitSource"]')
-        .type('taras')
-
-        .get('[data-qa-node="lastNamedebitSource"]')
-        .type('schevchenko')
-
-        .get('[data-qa-node="numberreceiver"]')
-        .type('5309233034765085')
-        .get('[data-qa-node="amount"]')
-        .type('300')
-        .get('[data-qa-node="toggle-comment"]')
-        .click()
-        .get('[data-qa-node="comment"]')
-        .type('Cypress test')
-        .get('button[type="submit"]')
-        .wait(3000)
-        .click()
-
-
-        .get('[data-qa-node="payer-card"]')
-        .should('have.text', '4552 3314 4813 8217')
-        .get('[data-qa-node="receiver-card"]')
-        .should('have.text', '5309 2330 3476 5085')
-        .get('[data-qa-node="payer-amount"]')
-        .should('have.text', '300 UAH')
-        .get('[data-qa-node="payer-currency"]')
-        .should('have.text', '85.30 UAH')
-        .get('[data-qa-node="receiver-amount"]')
-        .should('have.text', '300 UAH')
-        .get('[data-qa-node="receiver-currency"]')
-        .should('have.text', '0 UAH')
-        .get('[data-qa-node="total"]')
-        .find('b')
-        .eq(0)
-        .should('contain.text', '385.30')
-        .get('[data-qa-node="total"]')
-        .find('b')
-        .eq(1)
-        .should('contain.text', 'UAH')
-        .get('[data-qa-node="comment"]')
-        .should('have.text', 'Cypress test')
+    transfers.checkPayerCard('4552 3314 4813 8217')
+    transfers.checkRecieverCard('5309 2330 3476 5085')
+    transfers.checkPayerAmount('300 UAH')
+    transfers.checkPayerCurrency('85.30 UAH')
+    transfers.checkRecieverAmount('300 UAH')
+    transfers.checkRecieverCurrency('0 UAH')
+    transfers.checkTotal('385.30','UAH')
+    transfers.checkComment('Cypress test')
 })
---
+++ cypress/support/pages/transfers.js
@@ -0,0 +1,92 @@
+export class Transfers {
+
+    typeDebitCardData(cardNumber, expDate, cvv){
+        cy.get('[data-qa-node="numberdebitSource"]')
+            .type(cardNumber)
+            .get('[data-qa-node="expiredebitSource"]')
+            .type(expDate)
+            .get('[data-qa-node="cvvdebitSource"]')
+            .type(cvv)
+    }
+
+    typeName(firstName,lastName){
+        cy.get('[data-qa-node="firstNamedebitSource"]')
+            .type(firstName)
+            .get('[data-qa-node="lastNamedebitSource"]')
+            .type(lastName) 
+    }
+
+    typeReceiverCard(card) {
+        cy.get('[data-qa-node="numberreceiver"]')
+            .type(card)
+    }
+
+    typeAmount(amount){
+        cy.get('[data-qa-node="amount"]')
+            .type(amount)
+    }
+
+    typeComment(comment){
+        cy.get('[data-qa-node="toggle-comment"]')
+            .click()
+            .get('[data-qa-node="comment"]')
+            .type(comment)
+    }
+
+    submitPayment(){
+        cy.get('button[type="submit"]')
+            .click()
+    }
+
+    checkPayerCard(card) {
+        cy.get('[data-qa-node="payer-card"]')
+            .should('have.text', card)
+    }
+
+    checkRecieverCard(card) {
+        cy.get('[data-qa-node="receiver-card"]')
+            .should('have.text', card)
+    }
+
+    checkPayerAmount(amount) {
+        cy.get('[data-qa-node="payer-amount"]')
+            .should('have.text', amount)
+    }
+
+    checkPayerCurrency(currency) {
+        cy.get('[data-qa-node="payer-currency"]')
+            .should('have.text', currency)
+    }
+
+    checkRecieverAmount(amount) {
+        cy.get('[data-qa-node="receiver-amount"]')
+            .should('have.text', amount)
+    }
+
+    checkRecieverCurrency(currency) {
+        cy.get('[data-qa-node="receiver-currency"]')
+            .should('have.text', currency)
+    }
+
+    checkTotal(amount,currency){
+        cy.get('[data-qa-node="total"]')
+            .find('b')
+            .eq(0)
+            .should('contain.text', amount)
+
+            .get('[data-qa-node="total"]')
+            .find('b')
+            .eq(1)
+            .should('contain.text', currency)
+    }
+
+    checkComment(comment){
+        cy.get('[data-qa-node="comment"]')
+            .should('have.text', comment)
+    }
+
+
+
+}
+
+export const transfers = new Transfers()    
